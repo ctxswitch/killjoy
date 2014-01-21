71681447676cb77b7e66c36e1271fd1d6d4847b1
Rob Lyon <nosignsoflifehere@gmail.com>
2014-01-20 19:59:12 -0800

    wrong copyright. cut and paste does it again

--- killjoy.c
+++ killjoy.c
@@ -1,5 +1,5 @@
 /**
- * Copyright 2013, Institute for Bioninformatics and Evolutionary Studies
+ * Copyright 2013, Rob Lyon <nosignsoflifehere@gmail.com>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
